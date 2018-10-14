9427e10c113bd620e86f85d6f98ee5f3227f8048
Jennifer Hikichi <jenniferhikichi@Jennifers-MacBook-Pro.local>
2018-10-13 23:36:31 -1000

    one movie added

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@ const person4 = new XMLHttpRequest();
 function person4Listener (){
   if(this.readyState === 4){
     person4Name.innerHTML = JSON.parse(person4['responseText']).name;
-    console.log('this is this',this)
-    console.log(this.readyState)
-    console.log(this.responseText);
+    //console.log('this is this',this)
+    //console.log(this.readyState)
+    //console.log(this.responseText);
   }
 }
 person4.addEventListener('load',person4Listener);
@@ -36,3 +36,17 @@ function person14SpeciesElListener(){
 person14SpeciesEl.addEventListener('load', person14SpeciesElListener);
 person14SpeciesEl.open("GET","https://swapi.co/api/species/1/" )
 person14SpeciesEl.send();
+
+let starWars1 = document.createElement('li');
+starWars1.id ='A New Hope';
+starWars1.innerHTML;
+filmList.appendChild(starWars1);
+
+const swMovie1 = new XMLHttpRequest();
+
+function swMovie1Listener(){
+  starWars1.innerHTML = JSON.parse(swMovie1.responseText).results[0].title;
+}
+swMovie1.addEventListener('load',swMovie1Listener);
+swMovie1.open("GET","https://swapi.co/api/films/");
+swMovie1.send();
